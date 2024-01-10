aedee1551d56a4aae54759e78f9655ee3babbcf7
Sümeyye Güldemir <93578986+sumgul4@users.noreply.github.com>
2024-01-10 20:19:54 +0300

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,19 @@
+
+---------------------------------QUERIES--------------------------------------------
+
+--Seçilen ürüne verilen ortalama puanı listeler
+SELECT AVG(rate)
+FROM comments c, sales s
+WHERE s.saleID = c.saleID AND c.saleID = <istenenIDdegeriYazılacak> --??
+GROUP BY s.productID ;
+
+--Ortalama puanı 3'ten fazla olan ürünleri listeler
+SELECT p.productName, AVG(c.rate)
+FROM comments c, sales s, products p
+WHERE s.saleID = c.saleID AND s.productID = p.productID  --??
+GROUP BY p.productID 
+HAVING AVG(c.rate) > 3
+ORDER BY AVG(c.rate) desc
+
+--İstenen kategorideki ürünleri listeler
+SELECT * FROM products WHERE productCategory = 'jewelry';
---
+++ triggers.sql
@@ -0,0 +1,52 @@
+
+-------------------------------------TRIGGERS---------------------------------------
+
+-- Delete related comments
+--triggers when a user deletes a sale record
+CREATE OR REPLACE FUNCTION deleteRelatedComments()
+RETURNS TRIGGER AS $$
+BEGIN
+    DELETE FROM comments
+    WHERE saleID = OLD.saleID;  
+    RAISE NOTICE 'Comment for sale ID % has been deleted', OLD.saleID;
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER deleteRelatedCommentsTrigger
+AFTER DELETE 
+ON sales
+FOR EACH ROW 
+EXECUTE FUNCTION deleteRelatedComments();
+
+--Set purchase date of new sale with current date and time
+CREATE OR REPLACE FUNCTION update_purchase_date()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.purchasedate := LOCALTIMESTAMP;
+    RAISE NOTICE 'Purchase date for sale ID % has been updated.', NEW.saleID;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_update_purchase_date
+BEFORE INSERT ON sales
+FOR EACH ROW
+EXECUTE FUNCTION update_purchase_date();
+
+--Updates stock quantity when a product is inserted
+CREATE OR REPLACE FUNCTION updateQuantity()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE products
+    SET stockQuantity = stockQuantity + NEW.stockQuantity
+    WHERE productID = NEW.productID;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER updateQuantityTrigger
+AFTER INSERT ON products
+FOR EACH ROW 
+EXECUTE FUNCTION updateQuantity();
+
